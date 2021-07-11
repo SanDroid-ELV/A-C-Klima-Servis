3397054140f01b2d92d42838800b8d4c65479e48
SanDroid-ELV <86579337+SanDroid-ELV@users.noreply.github.com>
2021-07-11 23:08:51 +0200

    Create screen-detecttion.js

---
+++ JS/screen-detecttion.js
@@ -0,0 +1,10 @@
+window.addEventListener('resize', screen_resize);
+ 
+function screen_resize() {
+  var h = parseInt(window.innerHeight);
+  var w = parseInt(window.innerWidth);
+
+  if(w > 1080) {
+      window.location.replace("https://ac-klima-servis.xyz/desktop");
+  }
+ }
