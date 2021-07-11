712826df1fee287c2541605e4479dc364d5f79c5
SanDroid-ELV <86579337+SanDroid-ELV@users.noreply.github.com>
2021-07-11 23:37:27 +0200

    Update page-zoom.js

--- js/page-zoom.js
+++ js/page-zoom.js
@@ -1,3 +1,9 @@
-if (width >= 2736 ) {
-  document.getElementById('html').style.zoom = '25%';
-}
+function zoomOut(){
+                var body = document.querySelector("body");
+                var currWidth = body.clientWidth;
+                if(currWidth == 1920){
+                    alert("Maximum zoom-out level reached.");
+                } else{
+                    body.style.width = (currWidth - 25) + "px";
+                }
+            }
